acf12c594a1fcace63e6e2549e5bc5e97d49798c
Linas Spukas <linas.spukas@quantilope.com>
2021-01-31 21:18:09 +0200

    inject env variables; setup db config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { typeOrmConfig } from './config/typeorm.config';
+import { TypeOrmConfigService } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-    imports: [TypeOrmModule.forRoot(typeOrmConfig), TasksModule],
+    imports: [
+        ConfigModule.forRoot({ isGlobal: true }),
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule],
+            useClass: TypeOrmConfigService,
+        }),
+        TasksModule,
+    ],
 })
 export class AppModule {}
--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,21 @@
-import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
 
-export const typeOrmConfig: TypeOrmModuleOptions = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'tasks',
-    entities: [__dirname + '/../**/*.entity.{js,ts}'],
-    synchronize: true,
-};
+@Injectable()
+export class TypeOrmConfigService implements TypeOrmOptionsFactory {
+    constructor(private configService: ConfigService) {}
+
+    createTypeOrmOptions(): TypeOrmModuleOptions {
+        return {
+            type: 'postgres',
+            host: this.configService.get<string>('DB_HOST', 'localhost'),
+            port: this.configService.get<number>('DB_PORT', 5432),
+            username: this.configService.get<string>('DB_USERNAME', 'postgres'),
+            password: this.configService.get<string>('DB_PASSWORD', 'postgres'),
+            database: this.configService.get<string>('DB_DATABASE', 'tasks'),
+            entities: [__dirname + '/../**/*.entity.{js,ts}'],
+            synchronize: true,
+        };
+    }
+}
