b21e99073ccd5af0db2821b37e36e1430a228d92
Linas Spukas <linas.spukas@quantilope.com>
2021-02-06 10:52:34 +0200

    validate password

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Post,
+    UsePipes,
+    ValidationPipe,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
@@ -7,6 +13,7 @@ export class AuthController {
     constructor(private authService: AuthService) {}
 
     @Post('/signup')
+    @UsePipes(ValidationPipe)
     createUser(@Body() authCredentialsDto: AuthCredentialsDto): Promise<void> {
         return this.authService.signUp(authCredentialsDto);
     }
--- src/auth/dto/auth-credentials.dto.ts
+++ src/auth/dto/auth-credentials.dto.ts
@@ -1,9 +1,16 @@
-import { IsNotEmpty } from 'class-validator';
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class AuthCredentialsDto {
-    @IsNotEmpty()
+    @IsString()
+    @MinLength(4)
+    @MaxLength(15)
     username: string;
 
-    @IsNotEmpty()
+    @IsString()
+    @MinLength(5)
+    @MaxLength(15)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: 'Password too weak',
+    })
     password: string;
 }
