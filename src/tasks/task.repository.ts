d767bf24a77e967c026681689d12b1aa26620bcc
Linas Spukas <linas.spukas@quantilope.com>
2021-02-06 21:53:26 +0200

    add error log to task repo

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,3 +1,4 @@
+import { InternalServerErrorException, Logger } from '@nestjs/common';
 import { User } from 'src/auth/user.entity';
 import { EntityRepository, Repository } from 'typeorm';
 import { CreateTaskDto } from './dto/create-task.dto';
@@ -7,6 +8,8 @@ import { Task } from './task.entity';
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
+    private logger = new Logger('TaskRepository');
+
     async getAllTasks(
         filterTasksDto: FilterTasksDto,
         user: User,
@@ -28,7 +31,17 @@ export class TaskRepository extends Repository<Task> {
                 { search: `%${search}%` },
             );
 
-        return tasks.getMany();
+        try {
+            return await tasks.getMany();
+        } catch (error) {
+            this.logger.error(
+                `Failed to get tasks for user "${
+                    user.username
+                }". Filter: ${JSON.stringify(filterTasksDto)}`,
+                error.stack,
+            );
+            throw new InternalServerErrorException();
+        }
     }
 
     async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
@@ -40,7 +53,16 @@ export class TaskRepository extends Repository<Task> {
         task.status = TaskStatus.OPEN;
         task.user = user;
 
-        await task.save();
+        try {
+            await task.save();
+        } catch (error) {
+            this.logger.error(
+                `Failed to save task for user "${
+                    user.username
+                }". Task: ${JSON.stringify(createTaskDto)}`,
+                error.stack,
+            );
+        }
 
         // Return task to client without user field
         delete task.user;
