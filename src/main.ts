93a8870775cf2dac62cd9ca27991fed983cf4719
Linas Spukas <linas.spukas@quantilope.com>
2021-02-06 21:31:47 +0200

    logger: bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+    const logger = new Logger('bootstrap');
     const app = await NestFactory.create(AppModule);
-    await app.listen(3000);
+    const PORT = 3000;
+    await app.listen(PORT);
+    logger.log(`Server started on port ${PORT}`);
 }
 bootstrap();
